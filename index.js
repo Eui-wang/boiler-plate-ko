533ee6f3869362e94ccbbdb8872611c219cfa307
wang323@khu.ac.kr <wang323@khu.ac.kr>
2021-10-01 16:11:44 +0900

    security data

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = 9000
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+
+//application/json
+app.use(express.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://Euiwang:dhkd1598@boilerplate.jrgfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
 }).then(() => console.log('MongoDB Connected...')).catch(
     err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!!!'))   
+
+app.post('/register', (req, res) => {
+    //회원가입시 필요한 정보들을 cliet에서 가져오면
+    //Database에 넣는다.
+    const user = new User(req.body)
+    user.save((err, userinfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({ success: true})
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
